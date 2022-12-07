0a3c8081e91e78c36112914cd103776aad0a6e4d
arif-projects <arifulhaque60332@gmail.com>
2022-12-07 20:02:00 +0600

    ShoppingCart

---
+++ Module22/shoppingCart.js
@@ -0,0 +1,15 @@
+let products = [
+  { name: "shirt", price: 500, quantity: 3 },
+  { name: "pant", price: 1000, quantity: 3 },
+  { name: "shoe", price: 2500, quantity: 1 },
+  { name: "belt", price: 500, quantity: 2 },
+  { name: "watch", price: 3500, quantity: 1 },
+];
+
+let totalPrice = 0;
+for (let product of products) {
+  let productPrice = product.price + product.quantity;
+  totalPrice = totalPrice + productPrice;
+}
+
+console.log(totalPrice);
